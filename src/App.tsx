0045d5d19eb99a923cd37fbcefaeb7c27df243b5
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-04-01 20:12:37 +0700

    feature: add two 3D cartoon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import Typewriter from "typewriter-effect";
+import cartoonGirl from "./img/Cartoon-girl.png";
+import cartoonBoy from "./img/Cartoon-boy.png";
 
 function App() {
   return (
     <div className="App">
-      <div className="navbar"></div>
+      <div className="navbar">
+        <img className="image" src={cartoonGirl} alt="girl-cartoon" />
+
+        <img className="image" src={cartoonBoy} alt="boy-cartoon" />
+      </div>
       <form className="appForm">
         <div className="welcomeText">
           <Typewriter
